b5b2cdde92ae0c7e9a1cb3253cd7b6d4e57afc22
gkarumba <gachegua@gmail.com>
2019-08-11 10:50:59 +0300

    Add the business,profile & updates forms to the app/formss.py

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import *
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user','neighbourhood']
+        
+class UpdatesForm(forms.ModelForm):
+    class Meta:
+        models = Updates
+        exclude = ['author','neighbourhood','post_date']
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import *
+from .forms import *
+
 
 # Create your views here.
