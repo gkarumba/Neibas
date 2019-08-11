98fbccb9b27828c31bae31e55e2c1d1ee97b0f10
gkarumba <gachegua@gmail.com>
2019-08-11 09:47:32 +0300

    Add the necessary urls to the neibas/urls.py

--- neibas/urls.py
+++ neibas/urls.py
@@ -18,4 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('app.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page":'/'}),
 ]
