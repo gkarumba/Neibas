22038060e682727206c20128adafd179bec99b51
gkarumba <gachegua@gmail.com>
2019-08-11 10:14:44 +0300

    Register the models to the app/admin.py

--- app/admin.py
+++ app/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Profile,Neighbourhood,Business,Updates
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Updates)
