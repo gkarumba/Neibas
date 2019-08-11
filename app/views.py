eeea332944ab8d13bf70a573f86fc93df4b0a1e0
gkarumba <gachegua@gmail.com>
2019-08-11 10:58:45 +0300

    Add the home view,profile view and edit profile view to the app/views.py

--- app/views.py
+++ app/views.py
@@ -10,3 +10,31 @@ from .forms import *
 
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    try: 
+        current_user = request.user
+        profile = Profile.objects.get(user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('edit')
+    return render(request,'index.html')
+
+def profile(request):
+    current_user =request.user
+    profile=Profile.objects.get(user=current_user)
+    
+    return render(request,'profile.html',{'profile':profile})
+
+def edit_profile(request):
+    current_user=request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+    else:
+        form = ProfileForm()
+    return render(request,'edit_profile.html',{"form":form})
+            
