52f262bada6340eabec33fd7f8492099237a049f
gkarumba <gachegua@gmail.com>
2019-08-11 10:07:34 +0300

    Add the business model to the app/models.py

--- app/models.py
+++ app/models.py
@@ -57,4 +57,31 @@ class Profile(models.Model):
     def delete_profile(cls,username):
         cls.objects.filter(user=username).delete()
         
+class Business(models.Model):
+    '''
+    Model for the businesses
+    '''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    neibourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    biz_email = models.EmailField()
+    contact = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+    
+    def save_biz(self):
+        self.save()
+    
+    @classmethod
+    def delete_biz(cls,name):
+        cls.objects.filter(name=name).delete()
         
+    @classmethod
+    def update_biz(cls,biz,email,contact):
+        cls.objects.filter(name=biz).update(biz_email=email, contact=contact)
+        
+    @classmethod
+    def find_biz(cls,term):
+        result = cls.objects.filter(name__icontains=term)
+        return result
